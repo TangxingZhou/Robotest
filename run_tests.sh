062907ea518d599f7ae4c58ccfcbd9bd31b13ffa
Tangxing Zhou <tangxing.zhou@transwarp.io>
2019-06-05 18:10:19 +0800

    uncomment the executor

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@
 set -e
 
 python3 -m pip install --user -r requirements.txt
-# python3 -m run_robot $*
-#python3 -m run_robot --argumentfile runners/Demo/Internal_Chrome.txt
+python3 -m run_robot $*
+# python3 -m run_robot --argumentfile runners/Demo/Internal_Chrome.txt
