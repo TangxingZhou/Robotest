2d150b8e24d1a043b0e33c7d9ef8c69817200d92
Tangxing Zhou <tangxing.zhou@transwarp.io>
2019-06-12 15:57:17 +0800

    update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,9 @@
 @echo off
 
-python3 -m pip install --user -r requirements.txt
-python3 -m run_robot %*
+
+if "%1" == "--init" (
+    python3 -m pip install --user -r requirements.txt
+) else (
+    python3 -m run_robot %*
+)
 :: python3 -m run_robot --argumentfile runners/Demo/Internal_Chrome.txt
