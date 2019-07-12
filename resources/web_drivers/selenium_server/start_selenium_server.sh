a79ea676a9af2e53e9e8cf9da53c23edba8f41b7
Tangxing Zhou <tangxing.zhou@transwarp.io>
2019-07-12 16:06:00 +0800

    set the chromedriver path to start selenium server

--- resources/web_drivers/selenium_server/start_selenium_server.sh
+++ resources/web_drivers/selenium_server/start_selenium_server.sh
@@ -2,6 +2,7 @@
 
 set -x
 selenium_server_version=3.141.59
+chrome_driver_path=../chromedriver
 if [ -n "$1" ]; then
     selenium_server_logs_dir=$1
 else
@@ -10,4 +11,4 @@ fi
 
 nohup java -jar selenium-server-standalone-$selenium_server_version.jar -role hub -hubConfig hub_config.json > $selenium_server_logs_dir/selenium_server_hub.log &
 sleep 2
-nohup java -DWebdriver.chrome.driver=../chromedriver -jar selenium-server-standalone-$selenium_server_version.jar -role node -nodeConfig node_config.json > $selenium_server_logs_dir/selenium_server_node.log &
+nohup java -Dwebdriver.chrome.driver=$chrome_driver_path -jar selenium-server-standalone-$selenium_server_version.jar -role node -nodeConfig node_config.json > $selenium_server_logs_dir/selenium_server_node.log &
