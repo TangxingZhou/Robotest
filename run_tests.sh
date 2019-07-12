068471a667e710c97d30ace0015f5d15c66ecebd
Tangxing Zhou <tangxing.zhou@transwarp.io>
2019-07-12 15:39:31 +0800

    update start scripts

--- run_tests.sh
+++ run_tests.sh
@@ -2,20 +2,28 @@
 
 set -e
 exec_dir=`pwd`
+selenium_server_starter_dir=resources/web_drivers/selenium_server
 
-if [ "$1" = "--init" ]; then
-    python3 -m pip install --user -r requirements.txt
-elif [ "$1" = "--selenium-server" ]; then
-    if [ $(ps -ef | grep -v grep | grep selenium-server | wc -l) -ne 0 ]; then
-        for pid in $(pidof selenium-server)
+function terminate_service() {
+    if [ $(ps -ef | grep -v grep | grep $1 | wc -l) -ne 0 ]; then
+        for pid in $(ps -ef | grep -v grep | grep $1 | awk '{print $2}')
         do
+            echo $pid
             kill $pid
         done
         sleep 2
     fi
-    cd resources/web_drivers/selenium_server
+}
+
+if [ "$1" = "--init" ]; then
+    python3 -m pip install --user -r requirements.txt
+elif [ "$1" = "--sss" ]; then
+    terminate_service selenium-server
+    cd $selenium_server_starter_dir
     bash start_selenium_server.sh $exec_dir
     cd -
+elif [ "$1" = "--tss" ]; then
+    terminate_service selenium-server
 else
     python3 -m run_robot $*
 fi
